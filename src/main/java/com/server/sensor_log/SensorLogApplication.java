3eaca8af0c3f30a4afc61163ee8b9069a7e7cb8e
yago biermann <bsfyagobsf@gmail.com>
2026-04-19 00:26:57 -0300

    add springboot

---
+++ src/main/java/com/server/sensor_log/SensorLogApplication.java
@@ -0,0 +1,13 @@
+package com.server.sensor_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorLogApplication.class, args);
+	}
+
+}
