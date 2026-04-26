3d222eb106236f1ba4f3b9d0348395da306115bc
yago biermann <bsfyagobsf@gmail.com>
2026-04-26 03:07:55 -0300

    fix: remove repository from main application

--- src/main/java/com/server/sensor_log/SensorLogApplication.java
+++ src/main/java/com/server/sensor_log/SensorLogApplication.java
@@ -7,14 +7,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.server.sensor_log.mqtt.configs.MqttProperties;
-import com.server.sensor_log.repository.SensorRepository;
 
 @SpringBootApplication
 @EnableConfigurationProperties(MqttProperties.class)
 @EnableMongoRepositories
 public class SensorLogApplication {
-    @Autowired
-    SensorRepository sensorRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SensorLogApplication.class, args);
