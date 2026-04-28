a8dc244e4000ed26bc7982e29230e6cb546941fe
yago biermann <bsfyagobsf@gmail.com>
2026-04-27 23:02:29 -0300

    feat: add validate annotation

--- src/main/java/com/server/sensor_log/mqtt/TopicHandler.java
+++ src/main/java/com/server/sensor_log/mqtt/TopicHandler.java
@@ -1,10 +1,17 @@
 package com.server.sensor_log.mqtt;
 
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
 import jakarta.validation.constraints.NotBlank;
 
+import com.server.sensor_log.validations.ValidJson;
+
+@Component
+@Validated
 public interface TopicHandler {
 
     String getTopic();
 
-    void handle(@NotBlank String topic, @NotBlank String payload);
+    void handle(@NotBlank String topic, @NotBlank @ValidJson String payload);
 }
