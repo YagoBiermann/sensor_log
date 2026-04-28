ff820ff7c9c2303e2ce047db4d59632d0d899cc1
yago biermann <bsfyagobsf@gmail.com>
2026-04-27 22:56:14 -0300

    refactor: remove DeviceController related mappings

--- src/main/java/com/server/sensor_log/dto/LightMapper.java
+++ src/main/java/com/server/sensor_log/dto/LightMapper.java
@@ -2,7 +2,6 @@ package com.server.sensor_log.dto;
 
 import org.mapstruct.Mapper;
 
-import com.server.sensor_log.documents.DeviceController;
 import com.server.sensor_log.documents.Light;
 
 @Mapper(componentModel = "spring")
@@ -11,17 +10,4 @@ public interface LightMapper {
     LightPayloadDTO toDTO(Light light);
 
     Light toEntity(LightPayloadDTO dto);
-
-    default Integer map(DeviceController value) {
-        return value == null ? null : value.getValue();
-    }
-
-    default DeviceController map(Integer value) {
-        if (value == null) {
-            return null;
-        }
-        DeviceController device = new DeviceController();
-        device.setValue(value);
-        return device;
-    }
 }
