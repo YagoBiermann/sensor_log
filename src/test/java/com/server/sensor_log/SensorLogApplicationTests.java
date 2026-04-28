a2f42315c1a8ce74a42e1fdb1a84310e7c4052ae
yago biermann <bsfyagobsf@gmail.com>
2026-04-27 23:02:55 -0300

    refactor(test): remove mocking

--- src/test/java/com/server/sensor_log/SensorLogApplicationTests.java
+++ src/test/java/com/server/sensor_log/SensorLogApplicationTests.java
@@ -3,16 +3,10 @@ package com.server.sensor_log;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
-import com.server.sensor_log.services.MqttService;
 
 @SpringBootTest()
 @ActiveProfiles("test")
 class SensorLogApplicationTests {
-
-    @MockitoBean
-    private MqttService mqttService;
 
     @Test
     void contextLoads() {
