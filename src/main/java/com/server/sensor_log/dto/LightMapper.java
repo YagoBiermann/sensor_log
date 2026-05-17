be235f41e93e3a6a4f338dcd29a1db916a3c646c
yago biermann <bsfyagobsf@gmail.com>
2026-05-17 14:54:29 -0300

    refactor: use builder instead of setters when mapping DTO to entity

--- src/main/java/com/server/sensor_log/dto/LightMapper.java
+++ src/main/java/com/server/sensor_log/dto/LightMapper.java
@@ -1,10 +1,12 @@
 package com.server.sensor_log.dto;
 
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 
 import com.server.sensor_log.documents.Light;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = false), unmappedTargetPolicy = ReportingPolicy.WARN)
 public interface LightMapper {
 
     LightPayloadDTO toDTO(Light light);
