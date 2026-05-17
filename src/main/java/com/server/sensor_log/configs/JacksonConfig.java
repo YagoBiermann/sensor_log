3e57e92c751c4e29be3f29e2a08621e863455d15
yago biermann <bsfyagobsf@gmail.com>
2026-05-17 15:15:51 -0300

    feat: configure date mapping with jackson

--- src/main/java/com/server/sensor_log/configs/JacksonConfig.java
+++ src/main/java/com/server/sensor_log/configs/JacksonConfig.java
@@ -1,6 +1,9 @@
 package com.server.sensor_log.configs;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,13 @@ public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.getFactory()
+                .configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true)
+                .configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+        return mapper;
     }
 }
