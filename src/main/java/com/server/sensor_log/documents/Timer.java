44b7e6ea24ad3b3e2dbaa53e785855299b6565ca
Yago Biermann <bsfyagobsf@gmail.com>
2026-05-17 15:03:49 -0300

    refactor: Improve Timer class

--- src/main/java/com/server/sensor_log/documents/Timer.java
+++ src/main/java/com/server/sensor_log/documents/Timer.java
@@ -1,46 +1,92 @@
 package com.server.sensor_log.documents;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.DurationDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Period;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Valid
-public final class Timer {
+public class Timer {
 
     @NotBlank(message = "Timer name must not be blank")
     private String name = "Timer";
     @NotNull
-    @Min(value = 0, message = "Hours must be >= 0")
-    @Max(value = 23, message = "Hours must be <= 23")
-    private Integer hours = 0;
+    private Boolean enabled = false;
     @NotNull
-    @Min(value = 0, message = "Minutes must be >= 0")
-    @Max(value = 59, message = "Minutes must be <= 59")
-    private Integer minutes = 0;
+    private Duration duration = Duration.ZERO;
     @NotNull
-    private Boolean isActive = false;
+    private final LocalDateTime currentTime = LocalDateTime.now();
+    @NotNull
+    private Period daysActive = Period.ofDays(0);
 
-    public String getTimerStatus() {
-        if (hours == 0 && minutes == 0) {
-            return "OFF";
-        } else if (hours == 24 && minutes == 0) {
-            return "ON";
-        } else {
-            return "ON for " + hours + "h " + minutes + "m";
+    @AssertTrue(message = "Days should be between 0 and 7")
+    public boolean isPeriodValid() {
+        return daysActive != null && !daysActive.isNegative() && !(daysActive.getDays() > 7);
+    }
+
+    @AssertTrue(message = "Duration must be between 0 and 24 hours")
+    public boolean isDurationValid() {
+        return duration != null && !duration.isNegative() && duration.compareTo(Duration.ofHours(24)) <= 0;
+    }
+
+    public String getStatus() {
+        return enabled ? "ON" : "OFF";
+    }
+
+    public void setTimer(@Valid Duration duration, @Valid Period daysActive) {
+        this.duration = duration;
+        this.daysActive = daysActive;
+        if (duration.isPositive() && daysActive.getDays() >= 1) {
+            this.enabled = true;
         }
     }
 
-    @Override
-    public String toString() {
-        return "Timer{"
-                + "name='" + name + '\''
-                + ", hours=" + hours
-                + ", minutes=" + minutes
-                + '}';
+    public String ToString() {
+        return display();
+    }
+
+    public String display() {
+
+        LocalDateTime now = LocalDateTime.now();
+
+        return """
+                =========================
+                TIMER: %s
+                =========================
+                Status: %s
+                
+                Current Time: %s
+                Current Day: %s
+                
+                Cycle: %s
+                Timer ON for %d hours and %d days a week
+                
+                =========================
+                """.formatted(name, getStatus(), now.toLocalTime().withSecond(0).withNano(0), now.getDayOfWeek(), getCycle(), duration.toHours(), daysActive.getDays());
+    }
+
+    private String getCycle() {
+        long onHours = duration.toHours();
+        long offHours = 24 - onHours;
+
+        return onHours + "/" + offHours;
     }
 }
