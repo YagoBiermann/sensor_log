7ba0541ca1e43ec2939e2bdd80dd75516373e703
yago biermann <bsfyagobsf@gmail.com>
2026-05-17 15:05:38 -0300

    refactor: add SuperBuilder annotation

--- src/main/java/com/server/sensor_log/documents/Temperature.java
+++ src/main/java/com/server/sensor_log/documents/Temperature.java
@@ -1,28 +1,26 @@
 package com.server.sensor_log.documents;
 
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.validation.annotation.Validated;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
+@Data
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @TypeAlias("temperature")
 @Document(collection = "temperatures")
 @Validated
+@Slf4j
 public class Temperature extends Sensor {
 
     private Integer temperature = 0;     // °C
     private Integer humidity = 0;        // %
-    private Double ph = 0.0;               // pH
+    private Double ph = 0.0;             // pH
 
     @Override
     public String toString() {
