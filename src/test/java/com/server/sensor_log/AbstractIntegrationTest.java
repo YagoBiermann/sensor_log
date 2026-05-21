b285d7e3add7c23a24de1373ef7d373dd99d4af7
yago biermann <bsfyagobsf@gmail.com>
2026-05-21 01:01:23 -0300

    test: add base class for set up the services

---
+++ src/test/java/com/server/sensor_log/AbstractIntegrationTest.java
@@ -0,0 +1,90 @@
+package com.server.sensor_log;
+
+import com.hivemq.client.mqtt.MqttClient;
+import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
+import com.server.sensor_log.repository.SensorRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mongodb.MongoDBContainer;
+
+import java.time.Duration;
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("test")
+@DirtiesContext
+public abstract class AbstractIntegrationTest {
+    private static final Network network = Network.newNetwork();
+    @Container
+    static MongoDBContainer mongo =
+            new MongoDBContainer("mongo:7.0")
+                    .withReplicaSet()
+                    .withNetwork(network)
+                    .withNetworkAliases("mongo")
+                    .waitingFor(
+                            Wait.forListeningPort()
+                    );
+
+    @Container
+    static GenericContainer<?> mosquitto =
+            new GenericContainer<>("eclipse-mosquitto:2.0")
+                    .withExposedPorts(1883)
+                    .withNetwork(network)
+                    .withNetworkAliases("mqtt")
+                    .withClasspathResourceMapping(
+                            "mosquitto.conf",
+                            "/mosquitto/config/mosquitto.conf",
+                            BindMode.READ_ONLY
+                    )
+                    .waitingFor(Wait.forListeningPort());
+
+    protected static Mqtt5BlockingClient publisher;
+    @Autowired
+    protected SensorRepository repository;
+
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.mongodb.uri", () -> mongo.getReplicaSetUrl());
+
+        registry.add("mqtt.host", mosquitto::getHost);
+        registry.add("mqtt.port", () -> mosquitto.getMappedPort(1883));
+        registry.add("mqtt.clientId", () -> "sensor-log-test-client");
+    }
+
+    @BeforeEach
+    void cleanupDatabase() {
+        repository.deleteAll();
+    }
+
+    @BeforeAll
+    static void setupPublisher() {
+        publisher = MqttClient.builder()
+                .useMqttVersion5()
+                .identifier("test-publisher")
+                .serverHost(mosquitto.getHost())
+                .serverPort(mosquitto.getMappedPort(1883))
+                .buildBlocking();
+
+        publisher.connect();
+    }
+
+    @AfterAll
+    static void cleanupPublisher() {
+        if (publisher != null) {
+            publisher.disconnect();
+        }
+    }
+}
