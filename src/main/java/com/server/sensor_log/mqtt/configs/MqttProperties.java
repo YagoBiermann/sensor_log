f6b701f3caf2fe6e8a215f447060bf7288140206
yago biermann <bsfyagobsf@gmail.com>
2026-05-21 00:53:25 -0300

    feat: add validation annotations

--- src/main/java/com/server/sensor_log/mqtt/configs/MqttProperties.java
+++ src/main/java/com/server/sensor_log/mqtt/configs/MqttProperties.java
@@ -1,15 +1,26 @@
 package com.server.sensor_log.mqtt.configs;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
 
-@ConfigurationProperties(prefix = "mqtt")
 @Data
+@Validated
+@ConfigurationProperties(prefix = "mqtt")
 public class MqttProperties {
 
+    @NotBlank(message = "mqtt.host must not be blank")
     private String host;
+
+    @Min(value = 1, message = "mqtt.port must be greater than 0")
+    @Max(value = 65535, message = "mqtt.port must be lower than 65536")
     private int port;
+
+    @NotBlank(message = "mqtt.client-id must not be blank")
     private String clientId;
     private String username;
     private String password;
