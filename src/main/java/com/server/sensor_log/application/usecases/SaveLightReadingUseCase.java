848d7e46040715b06d829014fdc7965924d9a265
yago biermann <bsfyagobsf@gmail.com>
2026-05-26 00:05:39 -0300

    refactor: map and save LightReading entity

--- src/main/java/com/server/sensor_log/application/usecases/SaveLightReadingUseCase.java
+++ src/main/java/com/server/sensor_log/application/usecases/SaveLightReadingUseCase.java
@@ -1,5 +1,7 @@
 package com.server.sensor_log.application.usecases;
 
+import com.server.sensor_log.application.dto.LightMapper;
+import com.server.sensor_log.application.dto.LightReadingDTO;
 import com.server.sensor_log.application.ports.LightRepositoryPort;
 import com.server.sensor_log.domain.model.device.device_readings.LightReading;
 import lombok.extern.slf4j.Slf4j;
@@ -9,17 +11,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class SaveLightReadingUseCase {
     private final LightRepositoryPort lightRepositoryPort;
+    private final LightMapper lightMapper;
 
-    public SaveLightReadingUseCase(LightRepositoryPort lightRepositoryPort) {
+    public SaveLightReadingUseCase(LightRepositoryPort lightRepositoryPort, LightMapper lightMapper) {
         this.lightRepositoryPort = lightRepositoryPort;
+        this.lightMapper = lightMapper;
     }
 
-    public void execute(LightReading reading) {
+    public void execute(LightReadingDTO reading) {
         boolean foundLightDevice = lightRepositoryPort.findById(reading.getId()).isPresent();
         if(!foundLightDevice) {
             throw new RuntimeException("Could not find Light device: %s".formatted(reading.getId()));
         }
         log.debug("🔵 Saving light readings: {}", reading);
-        lightRepositoryPort.save(reading);
+        LightReading lightReading = lightMapper.toEntity(reading);
+        log.debug("🔵 Mapped LightPayloadDTO to Light entity: {}", lightReading);
+        lightRepositoryPort.save(lightReading);
     }
 }
