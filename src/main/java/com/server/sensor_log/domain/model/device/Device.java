a21342f7ae31f0683e676b110843b55f393e2f70
yago biermann <bsfyagobsf@gmail.com>
2026-05-25 21:11:15 -0300

    feat: add type and topic to device

--- src/main/java/com/server/sensor_log/domain/model/device/Device.java
+++ src/main/java/com/server/sensor_log/domain/model/device/Device.java
@@ -15,21 +15,28 @@ public abstract class Device {
     protected Instant readingTimestamp;
     protected Boolean active;
     protected String location;
+    protected DeviceType type = DeviceType.GENERIC;
+    protected String topic;
 
-    protected Device(String id, String location) {
-        validate(id, location);
+    protected Device(String id, String location, DeviceType type, String topic) {
+        validate(id, location, topic);
         this.id = id;
         this.readingId = id + "_" + readingTimestamp;
         this.readingTimestamp = Instant.now();
         this.location = location;
+        this.type = type;
+        this.topic = topic;
     }
 
-    private void validate(String id, String location) {
+    private void validate(String id, String location, String topic) {
         if (id == null || id.isBlank()) {
             throw new IllegalArgumentException("Id cannot be null or blank");
         }
         if (location == null || location.isBlank()) {
             throw new IllegalArgumentException("Location cannot be null or blank");
+        }
+        if (topic == null || topic.isBlank()) {
+            throw new IllegalArgumentException("Topic cannot be null or blank");
         }
     }
 }
