f462180202dc5e3a2c903fb468d0409f831a256b
yago biermann <bsfyagobsf@gmail.com>
2026-05-26 00:02:57 -0300

    refactor: rename repository

--- src/main/java/com/server/sensor_log/infra/repository/DeviceRepository.java
+++ src/main/java/com/server/sensor_log/infra/repository/DeviceRepository.java
@@ -3,6 +3,6 @@ package com.server.sensor_log.infra.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.server.sensor_log.domain.model.device.Device;
 
-public interface SensorRepository extends MongoRepository<Device, String> {
+public interface DeviceRepository extends MongoRepository<Device, String> {
     long countById(String id);
 }
