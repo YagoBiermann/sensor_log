1ef270e60db7b687bd46047a21bd9ca8c842ef7e
yago biermann <bsfyagobsf@gmail.com>
2026-05-27 01:02:18 -0300

    feat: add findById method to DeviceRepositoryPort

--- src/main/java/com/server/sensor_log/application/ports/DeviceRepositoryPort.java
+++ src/main/java/com/server/sensor_log/application/ports/DeviceRepositoryPort.java
@@ -2,6 +2,9 @@ package com.server.sensor_log.application.ports;
 
 import com.server.sensor_log.domain.model.device.Device;
 
+import java.util.Optional;
+
 public interface DeviceRepositoryPort {
     void save(Device device);
+    Optional<Device> findById(String deviceId);
 }
