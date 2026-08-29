0c3555316db4f5dead60c859d7e7231612974532
Yago Biermann <bsfyagobsf@gmail.com>
2026-08-28 23:01:05 -0300

    add mappings

--- src/main/java/com/server/sensor_log/application/mappers/DeviceMapper.java
+++ src/main/java/com/server/sensor_log/application/mappers/DeviceMapper.java
@@ -9,16 +9,21 @@ import org.mapstruct.*;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.WARN)
 public abstract class DeviceMapper {
 
+    @Mapping(target = "deviceId", source = "device.deviceId")
+    @Mapping(target = "readingIds", source = "device.readingIds")
+    @Mapping(target = "active", source = "device.active")
+    @Mapping(target = "topic", expression = "java(TopicParser.buildTopic(device.getLocation(), device.getSubLocation(), device.getType()))")
     public abstract DeviceDTO toDTO(Device device);
 
     public Device toEntity(DeviceDTO dto) {
         if (dto == null) return null;
-
+        
         TopicDTO topic = TopicParser.parse(dto.topic());
-
+        
         return mapToEntity(dto, topic);
     }
-
+    
+    @Mapping(target = "deviceId", source = "dto.deviceId")
     @Mapping(target = "subLocation", source = "topic.subLocation")
     @Mapping(target = "type", source = "topic.deviceType")
     protected abstract Device mapToEntity(DeviceDTO dto, TopicDTO topic);
