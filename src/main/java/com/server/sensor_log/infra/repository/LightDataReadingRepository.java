9738f42a97c39554370b5b1a331019ab8403dad5
Yago Biermann <bsfyagobsf@gmail.com>
2026-09-25 17:47:03 -0300

    remove predefined device

--- src/main/java/com/server/sensor_log/infra/repository/LightDataReadingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.server.sensor_log.infra.repository;
-
-import com.server.sensor_log.domain.model.device.device_readings.LightReading;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LightDataReadingRepository extends MongoRepository<LightReading, String> {
-}
